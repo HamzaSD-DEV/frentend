b91cfe1ff2b706e677639d273872498765c9022b
Hamza saoud <saoudh20@gmail.com>
2023-08-02 22:38:10 +0100

    Setting up project structure and implementing mui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App'
-import store from './redux/store'
+import store from './store/store'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { Provider } from 'react-redux'
 
 // As of React 18
