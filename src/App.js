b24b8c778c54664b7310b2cc5a02d5885c74ac59
Hamza saoud <saoudh20@gmail.com>
2023-08-05 22:01:50 +0100

    Adding drawer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavbarComponent from "./components/NavbarComponent";
 import {Login} from "./features/login/login";
+import ResponsiveDrawer from "./components/Drawer";
 
 
 function App() {
     return (
         <div className="App">
             <NavbarComponent/>
+            <ResponsiveDrawer/>
             <Login/>
         </div>
     );
